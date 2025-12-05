32cbd6ab5fea9b800405447c4be84e2aa803f01e
hongxi-wen <hongxi.wen@stonybrook.edu>
2025-12-05 15:48:28 -0500

    Removed unused DB initializer since we only use Mongo now

--- server/db/db.js
+++
@@ -1,8 +0,0 @@
-const dotenv = require('dotenv');
-const MongoDatabaseManager = require('./mongodb');
-const { PostgresDatabaseManager } = require('./postgresql')
-dotenv.config();
-
-const db = new DatabaseManager();
-
-module.exports = db 
