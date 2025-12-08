2859c383f1ae43979e3a9c671e2d7d60d0e32999
hongxi-wen <hongxi.wen@stonybrook.edu>
2025-12-07 22:42:09 -0500

    Updated App routes for each main screen

--- client/src/App.js
+++ client/src/App.js
@@ -1,35 +1,50 @@
 import './App.css';
 import { React } from 'react'
-import { BrowserRouter, Route, Switch } from 'react-router-dom'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import { AuthContextProvider } from './auth';
 import { GlobalStoreContextProvider } from './store'
 import {
     AppBanner,
-    HomeWrapper,
+    HomeScreen,
     LoginScreen,
-    RegisterScreen,
-    Statusbar,
-    WorkspaceScreen
+    CreateAccountScreen,
+    EditAccountScreen,
+    PlaylistsScreen,
+    SongsScreen
 } from './components'
 /*
   This is the entry-point for our application. Notice that we
   inject our store into all the components in our application.
   
-  @author McKilla Gorilla
 */
 const App = () => {   
     return (
+        // <BrowserRouter>
+        //     <AuthContextProvider>
+        //         <GlobalStoreContextProvider>              
+        //             <AppBanner />
+        //             <Switch>
+        //                 <Route path="/" exact component={HomeWrapper} />
+        //                 <Route path="/login/" exact component={LoginScreen} />
+        //                 <Route path="/register/" exact component={RegisterScreen} />
+        //                 <Route path="/playlist/:id" exact component={WorkspaceScreen} />
+        //             </Switch>
+        //             <Statusbar />
+        //         </GlobalStoreContextProvider>
+        //     </AuthContextProvider>
+        // </BrowserRouter>
         <BrowserRouter>
             <AuthContextProvider>
                 <GlobalStoreContextProvider>              
                     <AppBanner />
-                    <Switch>
-                        <Route path="/" exact component={HomeWrapper} />
-                        <Route path="/login/" exact component={LoginScreen} />
-                        <Route path="/register/" exact component={RegisterScreen} />
-                        <Route path="/playlist/:id" exact component={WorkspaceScreen} />
-                    </Switch>
-                    <Statusbar />
+                    <Routes>
+                        <Route path="/" element={<HomeScreen/>} />
+                        <Route path="/login" element={<LoginScreen/>} />
+                        <Route path="/register" element={<CreateAccountScreen/>} />
+                        <Route path="/edit" element={<EditAccountScreen/>} />
+                        <Route path="/playlists" element={<PlaylistsScreen/>} />
+                        <Route path="/songs" element={<SongsScreen/>} />
+                    </Routes>
                 </GlobalStoreContextProvider>
             </AuthContextProvider>
         </BrowserRouter>
